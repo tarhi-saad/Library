ae7f15aec6ed2528796ab29b30fc0198383896be
tarhi-saad <saad.trh@gmail.com>
2019-08-19 19:51:32 +0100

    Improving the design

--- src/index.js
+++ src/index.js
@@ -44,9 +44,9 @@ function render(books, row = null) {
       'beforeend',
       `
       <td>
-        <button class="remove-book">Remove</button>
+        <button class="edit-book green-btn">Edit</button>
         <button class="is-read">Is read ?</button>
-        <button class="edit-book">Edit</button>
+        <button class="remove-book">Delete</button>
       </td>
     `,
     );
@@ -63,9 +63,9 @@ function render(books, row = null) {
       <td>${book.pages}</td>
       <td>${book.read}</td>
       <td>
-        <button class="remove-book">Remove</button>
+        <button class="edit-book green-btn">Edit</button>
         <button class="is-read">Is read ?</button>
-        <button class="edit-book">Edit</button>
+        <button class="remove-book">Delete</button>
       </td>
     `;
     tr.insertAdjacentHTML('afterbegin', bookInfo);
@@ -80,20 +80,20 @@ function editBook(row) {
     <td><input type="text" id="title" name="title" size="1" value="${currentBook.title}"></td>
     <td><input type="text" id="author" name="author" size="1" value="${currentBook.author}"></td>
     <td><input type="number" id="pages" name="pages" min=0 value="${currentBook.pages}"></td>
-    <td align="center">${currentBook.read}</td>
+    <td>${currentBook.read}</td>
   `;
   const editControls = `
     <td class="edit-controls">
-      <button class="remove-book">Remove</button>
+      <button class="save green-btn">Save</button>
       <button class="is-read">Is read ?</button>
-      <button class="edit-ok">OK</button>
+      <button class="remove-book">Delete</button>
     </td>
   `;
   tr.insertAdjacentHTML('afterbegin', cells + editControls);
   tr.dataset.index = row.dataset.index;
   row.replaceWith(tr);
   tr.querySelector('input').focus();
-  tr.querySelector('.edit-ok').onclick = () => {
+  tr.querySelector('.save').onclick = () => {
     currentBook.title = tr.querySelector('#title').value;
     currentBook.author = tr.querySelector('#author').value;
     currentBook.pages = tr.querySelector('#pages').value;
@@ -113,12 +113,12 @@ newBookButton.onclick = () => {
     <td><input type="text" id="title" name="title" size="1"></td>
     <td><input type="text" id="author" name="author" size="1"></td>
     <td><input type="number" id="pages" name="pages" min=0></td>
-    <td align="center"><input type="checkbox" id="read" name="read"></td>
+    <td><input type="checkbox" id="read" name="read"></td>
   `;
   const editControls = `
     <td class="edit-controls">
-      <button class="edit-ok">OK</button>
-      <button class="edit-cancel">CANCEL</button>
+      <button class="edit-add  green-btn">Add</button>
+      <button class="edit-cancel">Delete</button>
     </td>
   `;
   tr.insertAdjacentHTML('afterbegin', cells + editControls);
@@ -128,7 +128,7 @@ newBookButton.onclick = () => {
     tr.remove();
   };
 
-  tr.querySelector('.edit-ok').onclick = () => {
+  tr.querySelector('.edit-add').onclick = () => {
     addBookToLibrary(tr);
     tr.dataset.index = myLibrary.length - 1;
     render([myLibrary[myLibrary.length - 1]], tr);
@@ -155,7 +155,7 @@ view.onclick = (event) => {
       readCell.innerHTML = myBook.read;
       break;
     }
-    case 'edit-book':
+    case 'edit-book green-btn':
       editBook(row);
       break;
     default:
